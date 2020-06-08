04a7efc28f1be140bbc0b32969b2893abc3488d2
oganexon <oganexon@protonmail.com>
2020-06-08 22:14:22 +0200

    Removed trash file

--- test.ts
+++
@@ -1,8 +0,0 @@
-// import { remove } from "https://deno.land/x/remove-forever/mod.ts";
-import { remove, fileStandards, directoryStandards } from "./mod.ts";
-
-remove("./trash", {
-  fileStandard: fileStandards.gutmann.remove,
-  directoryStandard: directoryStandards.unsafe.remove,
-  ignoreErrors: true,
-});
